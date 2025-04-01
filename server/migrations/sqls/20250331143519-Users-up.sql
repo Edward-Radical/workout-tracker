43427f6e7b9aaa6d6394d9a275a356257994d52f
Arianna <ari.pa97@hotmail.it>
2025-04-02 00:05:15 +0200

    Fix Migration of User table

--- server/migrations/sqls/20250331143519-Users-up.sql
+++ server/migrations/sqls/20250331143519-Users-up.sql
@@ -3,5 +3,8 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY, 
     email varchar(100) NOT NULL,
     password varchar(100) NOT NULL,
-    username varchar(100) NOT NULL
+    username varchar(100) NOT NULL,
+    "createdAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    "updatedAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    "deletedAt" TIMESTAMP
 );
