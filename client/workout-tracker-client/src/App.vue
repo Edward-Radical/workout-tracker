507089185165c38d70f6b64b0eb8a73221cb869d
Arianna <ari.pa97@hotmail.it>
2025-04-15 17:50:31 +0200

    Vue client init

---
+++ client/workout-tracker-client/src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+
+function login(){
+  console.log('login clicked');
+  
+}
+function signup(){
+  console.log('signup clicked');
+
+}
+</script>
+
+<template>
+  <main>
+    <div class="mobile-view">
+      <button @click=login()>Login</button>
+      <button @click=signup()>Signup</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.mobile-view{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  row-gap: 50px;
+}
+</style>
+
+
