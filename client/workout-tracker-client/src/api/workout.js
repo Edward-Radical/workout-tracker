1d5a59095c2c62398c8a5d565ea929b273c5f36f
Arianna <ari.pa97@hotmail.it>
2025-04-25 22:13:32 +0200

    add workouts api

--- client/workout-tracker-client/src/api/workout.js
+++ client/workout-tracker-client/src/api/workout.js
@@ -1,11 +1,14 @@
 import axiosInstance from './index'
 
-async function httpPostWorkout(reqBody) {
+const API_V = import.meta.env.VITE_API_VERSION;
+const API_URL = '/' + API_V + '/workouts' 
 
+async function httpGetWorkouts(dateFilter) {
+
+    let url = dateFilter ? API_URL + '?date=' + dateFilter : API_URL;
     try {
-        const { data } = await axiosInstance.post(
-            '/v1/workouts',
-            {reqBody},
+        const { data } = await axiosInstance.get(
+            url,
             {
                 withCredentials: true
             }
@@ -17,8 +20,50 @@ async function httpPostWorkout(reqBody) {
     } catch (error) {
         console.error(error.message);
     }
-    
+}
+async function httpGetWorkout(id) {
 
+    try {
+        const { data } = await axiosInstance.get(
+            API_URL + '/' + id,
+            {
+                withCredentials: true
+            }
+        ); 
+
+        if(data.success) return data.data;
+        else return false;
+
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+async function httpPostWorkout(reqBody) {
+
+    const { name, description, schedule, rep_days, user_id, notes, exercises_list} = reqBody
+    try {
+        const { data } = await axiosInstance.post(
+            API_URL,
+            {
+                name: name,
+                description: description,
+                schedule: schedule,
+                rep_days: rep_days,
+                user_id: user_id,
+                notes: notes,
+                exercises_list: exercises_list
+            },
+            {
+                withCredentials: true
+            }
+        ); 
+
+        if(data.success) return data.data;
+        else return false;
+
+    } catch (error) {
+        console.error(error.message);
+    }
 }
 
-export default { httpPostWorkout }
+export default { httpPostWorkout, httpGetWorkouts, httpGetWorkout }
