b17819a38b4a873a171f61cf2a3128939e906e6b
Arianna <ari.pa97@hotmail.it>
2025-04-26 23:42:53 +0200

    Fix workout validation check

--- server/src/validators/workoutValidation.ts
+++ server/src/validators/workoutValidation.ts
@@ -3,6 +3,7 @@ import { check } from 'express-validator';
 const validateWorkoutInput = [
     check('name')
         .notEmpty().withMessage('Name is required')
+        .isLength({ min: 1, max: 100 }).withMessage('Name must be between 2 and 100 characters')
 ];
 
 export default validateWorkoutInput;
